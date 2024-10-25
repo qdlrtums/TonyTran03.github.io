28c8fab77bcd96ef551df3bc1f843d80f9f1cf3b
TonyTran03 <tony.tran03@hotmail.com>
2024-10-25 04:48:31 -0400

    yes

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // This ensures Vite outputs to the 'dist' folder
+  },
   resolve: {
     alias: {
       "@": "/src",
     },
   },
+
   esbuild: {
     jsx: "automatic", // Or use 'react-jsx' if needed
   },
